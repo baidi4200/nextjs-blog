3089858642bdd2838129f6fd8be342b42de43343
baidi4200 <obaidobaid345@gmail.com>
2023-01-19 16:10:50 +0500

    some message to test

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: 'maximilian',
-        mongodb_password: '2YkcXq43KyPk0vqp',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'my-site-dev',
+        mongodb_username: "baidi420",
+        mongodb_password: "08LKdw4XCiRrEPHA",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "nextjs",
       },
     };
   }
 
   return {
     env: {
-      mongodb_username: 'maximilian',
-      mongodb_password: '2YkcXq43KyPk0vqp',
-      mongodb_clustername: 'cluster0',
-      mongodb_database: 'my-site',
+      mongodb_username: "maximilian",
+      mongodb_password: "2YkcXq43KyPk0vqp",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-site",
     },
   };
 };
